28737c53fe35b9d7378a62ef93c2d9d80a4ba617
Kaho <kahochan21@gmail.com>
2023-10-30 23:29:53 +0200

    completed Lesson 9. Changes Documented in ReadMe

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
 import Room from './Room';
+import { FrontPage } from './FrontPage';
+
 import {
     BrowserRouter as Router,
     Routes,
     Route,
     Link,
     Redirect,
+    Navigate,
 } from "react-router-dom";
 
 
 const HomePage = () => {
-  return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<h1>This is the Home Page</h1>} />
-              <Route path="/join" element={<RoomJoinPage />} />
-              <Route path="/create" element={<CreateRoomPage />} />
-              <Route path="/room/:roomCode" element={<Room />} />
-          </Routes>
-      </Router>
-  )
+
+    const [roomInfo, setRoomInfo] = useState({
+        roomCode: null,
+    });
+
+    useEffect(() => {
+        fetch('/api/user-in-room')
+        .then((res) => res.json())
+        .then((data) => {
+            setRoomInfo({
+                roomCode: data.code
+            })
+        });
+    }, [])
+
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={roomInfo.roomCode ? (<Navigate replace to={`/room/${roomInfo.roomCode}`} />) : (<FrontPage />)} />
+                <Route path="/join" element={<RoomJoinPage />} />
+                <Route path="/create" element={<CreateRoomPage />} />
+                <Route path="/room/:roomCode" element={<Room />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default HomePage
