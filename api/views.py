91967ff5df20ede5cdc1a669ba0021a634a115e2
Kaho <kahochan21@gmail.com>
2023-11-13 22:54:40 +0200

    Updated Python code  Please enter the commit message for your changes. Lines starting

--- api/views.py
+++ api/views.py
@@ -98,13 +98,12 @@ class LeaveRoom(APIView):
                 room.delete()
 
         return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+    
 
-class UpdateView(APIView):
-
+class UpdateRoom(APIView):
     serializer_class = UpdateRoomSerializer
 
     def patch(self, request, format=None):
-
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
 
@@ -116,16 +115,16 @@ class UpdateView(APIView):
 
             queryset = Room.objects.filter(code=code)
             if not queryset.exists():
-                return Response({'msg': 'Room not found'}, status=status.HTTP_404_NOT_FOUND)
-            
+                return Response({'msg': 'Room not found.'}, status=status.HTTP_404_NOT_FOUND)
+
             room = queryset[0]
             user_id = self.request.session.session_key
             if room.host != user_id:
-                return Response({'msg': 'You are not the host of this room'}, status=status.HTTP_403_FORBIDDEN)
-            
+                return Response({'msg': 'You are not the host of this room.'}, status=status.HTTP_403_FORBIDDEN)
+
             room.guest_can_pause = guest_can_pause
             room.votes_to_skip = votes_to_skip
-            room.save(update_fields={'guest_can_pause', 'votes_to_skip'})
+            room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
             return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
 
         return Response({'Bad Request': "Invalid Data..."}, status=status.HTTP_400_BAD_REQUEST)
